1d3f041dfc80b06cc78c04eab0146392a87bacd6
dengchengHu <991594112@qq.com>
2024-05-27 23:11:10 +0800

    [new commit] update account data structure

--- types/account.go
+++ types/account.go
@@ -8,17 +8,9 @@ type Account struct {
 	PrivateKey string `json:"private_key"`
 	Address    string `json:"address"`
 	Balance    int64  `json:"balance"`
-	Nonce      int64  `json:"nonce"`
+	IntraNonce int64  `json:"intra_nonce"`
+	InterNonce int64  `json:"inter_nonce"`
 }
-
-//func (a *Account) RLPEncode() ([]byte, error) {
-//	encoded, err := rlp.EncodeToBytes(a)
-//	fmt.Println(string(encoded))
-//	if err != nil {
-//		return nil, err
-//	}
-//	return encoded, nil
-//}
 
 func (a *Account) Marshal() ([]byte, error) {
 	encoded, err := json.Marshal(a)
@@ -27,14 +19,6 @@ func (a *Account) Marshal() ([]byte, error) {
 	}
 	return encoded, nil
 }
-
-//func (a *Account) RLPDecode(content []byte) error {
-//	err := rlp.DecodeBytes(content, a)
-//	if err != nil {
-//		return err
-//	}
-//	return nil
-//}
 
 func (a *Account) Unmarshal(content []byte) error {
 	err := json.Unmarshal(content, a)
